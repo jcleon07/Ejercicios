12a05270ad4ddfb30aaa925fc38811e36346dcbf
jcleon07 <jcleon0722@gmail.com>
2026-04-24 07:58:43 -0500

    Suma de dos elementos de un array

---
+++ Leet/sum_two_inArr.c
@@ -0,0 +1,18 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+
+    int* result = malloc(2*sizeof(int));
+
+    for(int i = 0; i < numsSize; i++){
+        int j = i+1;
+        while( j < numsSize){
+            if (nums[i] + nums[j] == target){
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            } 
+            j++;
+        }
+    }
+    return NULL;
+}
