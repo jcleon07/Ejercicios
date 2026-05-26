95b7133b14bebbcd2aeeed36a48e198bf5ffc193
Juan López <jcleon0722@gmail.com>
2026-05-26 17:17:34 -0500

    left and right rotate
    Added left and right rotation functions for red-black tree.

--- Algoritmos/arbol_rojinegro.py
+++ Algoritmos/arbol_rojinegro.py
@@ -13,4 +13,41 @@ class RBTree:
         self.nil.left = None
         self.nil.right = None
         self.root = self.nil
-        
+    
+def left_rotate(T,x):
+    y = x.right
+    x.right = y.left
+
+    if y.left is not None:
+        y.left.parent = x
+
+    y.parent = x.parent
+
+    if x.parent == None:
+        T.root = y
+    elif x == x.parent.left:
+        x.parent.left = y
+    else:
+        x.parent.right = y
+
+    y.left = x
+    x.parent = y
+
+def right_rotate(T,x):
+    y = x.left
+    x.left = y.right
+
+    if y.right is not None:
+        y.right.parent = x
+    
+    y.parent = x.parent 
+
+    if x.parent == None:
+        T.root = y
+    elif x == x.parent.right:
+        x.parent.right = y
+    else:
+        x.parent.left = y
+
+    y.right = x
+    x.parent = y
