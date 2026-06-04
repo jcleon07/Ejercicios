aac5ad2e0679142eda901618a362fd5a64c42c36
jcleon07 <jcleon0722@gmail.com>
2026-06-04 12:21:25 -0500

    Methods RB Tree

--- Algoritmos/arbol_rojinegro.py
+++ Algoritmos/arbol_rojinegro.py
@@ -4,7 +4,7 @@ class RBNode:
         self.left = None
         self.right = None
         self.parent = None
-        self.color = 1
+        self.color = 0
 
 class RBTree:
     def __init__(self):
@@ -14,7 +14,7 @@ class RBTree:
         self.nil.right = None
         self.root = self.nil
     
-    def left_rotate(T,x):
+    def left_rotate(self, x):
         y = x.right
         x.right = y.left
 
@@ -24,7 +24,7 @@ class RBTree:
         y.parent = x.parent
 
         if x.parent == None:
-            T.root = y
+            self.root = y
         elif x == x.parent.left:
             x.parent.left = y
         else:
@@ -33,7 +33,7 @@ class RBTree:
         y.left = x
         x.parent = y
 
-    def right_rotate(T,x):
+    def right_rotate(self, x):
         y = x.left
         x.left = y.right
 
@@ -43,7 +43,7 @@ class RBTree:
         y.parent = x.parent 
 
         if x.parent == None:
-            T.root = y
+            self.root = y
         elif x == x.parent.right:
             x.parent.right = y
         else:
@@ -53,125 +53,164 @@ class RBTree:
         x.parent = y
 
 
-    def insert(self, val):
+        
 
-        # Ordinary Binary Search Insertion
+    def insert(self, z):
+        y = self.nil
+        x = self.root
+        
+        while x != self.nil:
+            y = x
 
-        new_node = RBNode(val)
-
-        new_node.parent = None
-
-        new_node.left = self.nil
-
-        new_node.right = self.nil
-
-        new_node.red = True  # new node must be red
-
-
-
-        parent = None
-
-        current = self.root
-
-        while current != self.nil:
-
-            parent = current
-
-            if new_node.val < current.val:
-
-                current = current.left
-
-            elif new_node.val > current.val:
-
-                current = current.right
-
+            if z.key == x.key:
+                x = x.left
             else:
+                x.right
+        z.parent = y
 
-                return
-
-        # Set the parent and insert the new node
-
-        new_node.parent = parent
-
-        if parent == None:
-
-            self.root = new_node
-
-        elif new_node.val < parent.val:
-
-            parent.left = new_node
-
+        if y == self.nil:
+            self.root = z
+        elif z.key < y.key:
+            y.left = z
         else:
+            y.right = z
 
-            parent.right = new_node
+        z.left = self.nil
+        z.right = self.nil
+        z.color = 1
+        self.insert_fixup(self,z)
 
-        # Fix the tree
+    def insert_fixup(self, z):
+        while z.parent.color == 1:
+            if z.parent == z.parent.parent.left:
+                y = z.parent.parent.right
 
-        self.fix_insert(new_node)
+                if y.color == 1:
+                    z.parent.color = 0
+                    y.color = 0
+                    z.parent.parent.color = 1 
+                    z = z.parent.parent
+                elif z == z.parent.right:
+                    z = z.parent
+                    self.left_rotate(self,z)
 
-    def fix_insert(self, new_node):
-
-        while new_node != self.root and new_node.parent.red:
-
-            if new_node.parent == new_node.parent.parent.right:
-
-                u = new_node.parent.parent.left  # uncle
-
-                if u.red:
-
-
-
-                    u.red = False
-
-                    new_node.parent.red = False
-
-                    new_node.parent.parent.red = True
-
-                    new_node = new_node.parent.parent
-
-                else:
-
-                    if new_node == new_node.parent.left:
-
-                        new_node = new_node.parent
-
-                        self.rotate_right(new_node)
-
-                    new_node.parent.red = False
-
-                    new_node.parent.parent.red = True
-
-                    self.rotate_left(new_node.parent.parent)
-
+                z.parent.color = 0
+                z.parent.parent.color = 1
+                self.right_rotate(self, z.parent.parent)
+            
             else:
+                y = z.parent.parent.left
 
-                u = new_node.parent.parent.right  # uncle
+                if y.color == 1:
+                    z.parent.color = 0
+                    y.color = 0
+                    z.parent.parent.color = 1
+                    z = z.parent.parent
+                elif z == z.parent.left:
+                    z = z.parent
+                    self.right_rotate(self,z)
 
+                z.parent.color = 0
+                z.parent.parent.color = 1
+                self.left_rotate(self, z.parent.parent)
 
+        self.root.color = 0
 
-                if u.red:
+    def transplant(self, u, v):
+        if u.parent == self.nil:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
 
-                    u.red = False
+        v.parent = u.parent 
 
-                    new_node.parent.red = False
+    def tree_minimum(x):
+        while x.left != None:
+            x = x.left
 
-                    new_node.parent.parent.red = True
+        return x
 
-                    new_node = new_node.parent.parent
+    def delete(self, z):
+        y = z
+        y_original_color = y.color
 
-                else:
+        if z.left == self.nil:
+            x = z.right
+            self.transplant(self, z, z.right)
+        elif z.right == self.nil:
+            x = z.left
+            self.transplant(self, z, z.left)
+        else:
+            y = self.tree_minimum(z.right)
+            y_original_color = y.color
+            x = y.right
 
-                    if new_node == new_node.parent.right:
+            if y != z.right:
+                self.transplant(self, y, y.right)
+                y.right = z.right
+                y.right.parent = y
+            else:
+                x.parent = y 
+            
+            self.transplant(self, z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+        
+        if y_original_color == 0:
+            self.delete_fixup(self, x)
 
-                        new_node = new_node.parent
+    def delete_fixup(self, x):
+        if x == x.parent.left:
+            w = x.parent.right
 
-                        self.rotate_left(new_node)
+            if w.color == 1:
+                w.color = 0
+                x.parent.color = 1
+                self.left_rotate(self, x.parent)
+                w = x.parent.right
 
-                    new_node.parent.red = False
+            if w.left.color == 0 and w.right.color == 0:
+                w.color = 1
+                x = x.parent
+            else:
+                if w.right.color == 0:
+                    w.left.color = 0
+                    w.color = 1
+                    self.right_rotate(self, w)
+                    w = x.parent.right
+                
+                w.color = x.parent.color
+                x.parent.color = 0
+                w.right.color = 0
+                self.left_rotate(self, x.parent)
+                x = self.root
+        
+        else:
+            w = x.parent.left
 
-                    new_node.parent.parent.red = True
+            if w.color == 1:
+                w.color = 0
+                x.parent.color = 1
+                self.right_rotate(self, x.parent)
+                w = x.parent.left
 
-                    self.rotate_right(new_node.parent.parent)
-
-        self.root.red = False
-
+            if w.right.color == 0 and w.left.color == 0:
+                w.color = 1
+                x = x.parent
+            else:
+                if w.left.color == 0:
+                    w.right.color = 0
+                    w.color = 1
+                    self.left_rotate(self, w)
+                    w = x.parent.left
+                
+                w.color = x.parent.color
+                x.parent.color = 0
+                w.left.color = 0
+                self.right_rotate(self, x.parent)
+                x = self.root
+                
+        x.color = 0
